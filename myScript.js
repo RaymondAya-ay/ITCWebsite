ce61f8312388638cf82b748cefa50f86a1a3cd80
Raymond Aya-ay <raymond.p.ayaay@gmail.com>
2019-12-08 23:25:31 +0800

    almost done

--- myScript.js
+++ myScript.js
@@ -3,9 +3,17 @@ function darkMode(){
 	if(checkBox.checked == true){
 		document.body.style.backgroundColor = "#083754";
 		document.body.style.color = "#FFFFFF";
+		document.getElementById("homelink").href = "indexDark.html";
+		document.getElementById("phonelink").href = "phoneWallpapersDark.html";
+		document.getElementById("desktoplink").href = "desktopWallpapersDark.html";
+		document.getElementById("profilelink").href = "profilePicturesDark.html";
 	}
 	else{
 		document.body.style.backgroundColor = "#FFFFFF";
 		document.body.style.color = "#000000";
+		document.getElementById("homelink").href = "index.html";
+		document.getElementById("phonelink").href = "phoneWallpapers.html";
+		document.getElementById("desktoplink").href = "desktopWallpapers.html";
+		document.getElementById("profilelink").href = "profilePictures.html";
 	}
 }
